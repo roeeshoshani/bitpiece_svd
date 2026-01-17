aa4600e78746394298eb49fa74aa84ef93bbf151
Roee Shoshani <roysho10@gmail.com>
2026-01-18 01:29:34 +0200

    minor cleanup

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,8 @@ fn main() -> Result<()> {
     let xml = fs::read_to_string(&args.svd)
         .with_context(|| format!("reading SVD: {}", args.svd.display()))?;
 
-    // 1) Parse an "expanded" device: resolves derivedFrom, expands arrays, fills inherited props.
-    //    This is what we use for codegen correctness.
     let dev = svd_ir::parse_expanded(&xml)?;
-
-    // 2) Build an IR that groups derived peripherals and decides shared register blocks.
     let ir = svd_ir::build_device_ir(&dev)?;
-
-    // 3) Emit crate: Cargo.toml, lib.rs, and one module per group
     generate::emit_crate(&args.out, &ir)?;
 
     eprintln!("Generated crate at: {}", args.out.display());
